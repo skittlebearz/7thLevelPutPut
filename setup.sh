9187b7dab38bd3a7121513dd9e62ddd9566ce4de
jonscoresby <me@jonscoresby.com>
2021-10-13 15:20:28 -0600

    changed pythonvenv package be more generic

--- setup.sh
+++ setup.sh
@@ -47,7 +47,7 @@ db_django_password=`openssl rand -base64 14`
 # Script proper
 
 # Download and install dependencies
-yes y | sudo apt install python3 python3-pip python3-dev python3.8-venv default-libmysqlclient-dev build-essential
+yes y | sudo apt install python3 python3-pip python3-dev python3-venv default-libmysqlclient-dev build-essential
 wget -c https://repo.mysql.com//mysql-apt-config_0.8.19-1_all.deb
 sudo DEBIAN_FRONTEND=noninteractive dpkg -i mysql-apt-config_0.8.19-1_all.deb
 yes y | sudo DEBIAN_FRONTEND=noninteractive apt install mysql-server
