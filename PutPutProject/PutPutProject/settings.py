85468eab129a6896d6fea3dd71365fe4c5559b3e
jonscoresby <me@jonscoresby.com>
2021-10-27 19:28:44 -0600

    added sqlite settings back but commented out

--- PutPutProject/PutPutProject/settings.py
+++ PutPutProject/PutPutProject/settings.py
@@ -82,6 +82,12 @@ DATABASES = {
             'read_default_file': str(BASE_DIR / 'mysql.cnf')
             }
     }
+
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+
 }
 
 
