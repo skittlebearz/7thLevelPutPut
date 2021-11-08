1fe4e36f5b885e8bb9be12b5e016abfa2fdc50fa
Jordan Taylor <a01656724@usu.edu>
2021-11-08 11:27:51 -0700

    updated drink menu layout

--- PutPutProject/PutPutApp/models.py
+++ PutPutProject/PutPutApp/models.py
@@ -5,16 +5,50 @@ from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_type= models.CharField(max_length=1)
+    # user_type= models.CharField(max_length=1)
+    firstname = models.CharField(max-length=30)
+    lastname = models.CharField(max-length=30)
+    # TODO: check for users with same name. Maybe unique=True?
+    # username = models.CharField(user_firstname + " " + user_lastname)
+
+    user_type = models.CharField(blank=True, choices=UserTypes.choices, max_length=1)
+    UserTypes = models.TextChoices('User Type', 'Player Bartender Sponsor Manager')
+
+    # allow users to be multiple types simultaneously
+    player = models.BooleanField(default=True) # default user type
+    bartender = models.BooleanField(default=False) # admin approved by Manager
+    sponsor = models.BooleanField(default=False) # admin approved by Manager
+    manager = models.BooleanField(default=False) # a superuser
+
     account_balance = models.DecimalField(decimal_places=2, max_digits=12, default=0)
+
+    def get_firstname(self):
+        return self.firstname
+
+    @property
+    def is_player(self):
+        return self.player
+
+    @property
+    def is_bartender(self):
+        return self.bartender
+
+    @property
+    def is_sponsor(self):
+        return self.sponsor
+
+    @property
+    def is_manager(self):
+        return self.manager
 
 class Score(models.Model):
     user = models.ManyToManyField(User)
     day = models.DateField()
     num_strokes = models.IntegerField()
     hole = models.IntegerField()
-    
+
 class Drink(models.Model):
+    # TODO: get username from Profile
     name = models.CharField(max_length=30)
     cost = models.DecimalField(decimal_places=2, max_digits=6)
     description = models.CharField(max_length=240)
@@ -23,6 +57,7 @@ class Drink(models.Model):
         return self.name
 
 class Orders(models.Model):
+    # TODO: get username from Profile
     name = models.CharField(max_length=30)
     drink = models.CharField(max_length=240)
     location = models.PositiveIntegerField()
@@ -39,4 +74,3 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
-
