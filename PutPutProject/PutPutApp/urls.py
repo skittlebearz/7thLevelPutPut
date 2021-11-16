ef3026e09d0a208520d9449699312f8462a1a12a
Jordan Taylor <a01656724@usu.edu>
2021-11-16 12:32:26 -0700

    added link to home on empty address

--- PutPutProject/PutPutApp/urls.py
+++ PutPutProject/PutPutApp/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from PutPutApp.views import dashboard, register, menu, manage_menu, orders, login, fulfill_order, leaderboard
 
 urlpatterns = [
+    path("", dashboard, name="dashboard"),
+    path("dashboard/", dashboard, name="dashboard"),
     path("register/", register, name="register"),
     path("login/", login, name="login"),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("dashboard/", dashboard, name="dashboard"),
     path("menu/", menu, name="menu"),
     path("managemenu/", manage_menu, name="manage_menu"),
     path("orders/", orders, name="orders"),
