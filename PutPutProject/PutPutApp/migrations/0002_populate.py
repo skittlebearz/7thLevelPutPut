aa14d197d3b49a1ffd3a349d15f26abb7b6b9f56
Jordan Taylor <a01656724@usu.edu>
2021-11-17 22:53:32 -0700

    added some drinks and queue items

--- PutPutProject/PutPutApp/migrations/0002_populate.py
+++ PutPutProject/PutPutApp/migrations/0002_populate.py
@@ -14,7 +14,7 @@ def populate_db(apps,schema_editor):
     drink_a = Drink(
             name="Dirty Diet Coke",
             cost="2",
-            description="Diet coke with a shot of cocunut"
+            description="Diet coke with a shot of coconut"
             )
     drink_a.save()
 
@@ -53,6 +53,27 @@ def populate_db(apps,schema_editor):
             )
     drink_f.save()
 
+    drink_g = Drink(
+        name="Honey Mead",
+        cost="4.75",
+        description="(Non-alchoholic) Sparkling water, honey, apple, and cinnamon"
+    )
+    drink_g.save()
+
+    drink_h = Drink(
+        name="Currant Melomel",
+        cost="2.45",
+        description="(Non-alchoholic) Black currant juice with honey and spices"
+    )
+    drink_h.save()
+
+    drink_i = Drink(
+        name="Aqua Vitae",
+        cost="1.20",
+        description="Sparkling water"
+    )
+    drink_i.save()
+
 
 
 
@@ -89,6 +110,19 @@ def populate_db(apps,schema_editor):
             )
     order_d.save()
 
+    order_e = Orders(
+            name="Ambrose",
+            drink="Honey Mead",
+            location="9"
+            )
+    order_e.save()
+
+    order_f = Orders(
+            name="Viola",
+            drink="Currant Melomel",
+            location="3"
+            )
+    order_f.save()
 
 
     sr_a = SponsorRequest(
@@ -131,7 +165,7 @@ def populate_db(apps,schema_editor):
 
     tournament_a = Calendar(
             day=datetime.date(2022,1,9),
-            tournament_name="Campell Soup Cup"
+            tournament_name="Campbell Soup Cup"
             )
     tournament_a.save()
 
